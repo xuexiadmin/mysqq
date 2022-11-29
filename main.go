20b35fd5fc384170b1acc790abe091d52ea31293
ghz <ghz@email.com>
2022-11-29 09:37:23 +0800

    v4

---
+++ main.go
@@ -0,0 +1,28 @@
+// package main
+// import "fmt"
+// func main() {
+// 	const PU float64=3.141
+// 	const PI = 3.12333
+// 	const (
+// 		width =100
+// 		gg int =1000
+// 		height=200
+// 	)
+// 	const(
+// 		a1=iota
+// 		a2=99
+// 		_
+// 		a3=iota
+// 		a4=iota
+// 		a5=iota
+// 	)
+// 	const i,j = 1,2
+// 	fmt.Printf("PI: %v\n",PU)
+// 	fmt.Printf("PI: %v\n",PI)
+// 	fmt.Printf("gg: %v",gg)
+// 	fmt.Printf("a1: %v\n",a1)
+// 	fmt.Printf("a1: %v\n",a2)
+// 	fmt.Printf("a1: %v\n",a3)
+// 	fmt.Printf("a1: %v\n",a4)
+// 	fmt.Printf("a1: %v\n",a5)
+// }
